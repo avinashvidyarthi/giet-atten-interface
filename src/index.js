435e4655522068b5c50c8db13bcd2a6d56e5ed59
avinashvidyarthi <avinashvidyaryhi@gmail.com>
2019-11-01 01:22:49 +0530

    ss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 var isLoading = false;
 
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/sw.js').then(function(){
+    navigator.serviceWorker.register('/giet-atten-interface/sw.js').then(function(){
         console.log("Service worker registered");
     });
 }
